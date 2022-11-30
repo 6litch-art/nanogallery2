d7f17c42810fa214fdc73e9fa51a2aea97d6e254
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-11-30 10:00:29 +0100

    Update nanogallery2

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import 'shifty';
+import 'hammerjs';
+import 'imagesloaded';
+import 'screenfull';
+
+import "./scss/index.scss"
+
+// import 'nanogallery2/src/jquery.nanogallery2.core.js';
+import 'nanogallery2/dist/jquery.nanogallery2.js';
